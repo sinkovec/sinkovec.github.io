ff37a214a1ebe0053706d94773fc17422f896a9f
Brian Sinkovec <bsin1807@gmail.com>
2023-12-12 16:32:57 +0100

    css adjustments for landingpage

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="flex flex-col space-y-4 text-xl">
-    <img src="/assets/img/bio-photo.png" class="block mx-auto w-1/2" />
-    <p>
-      Hi, my name is Brian Sinkovec and I'm an enthusiastic software engineer
-      from Aachen, Germany. I graduated from
-      <NuxtLink to="https://www.rwth-aachen.de"
-        >RWTH Aachen University
-      </NuxtLink>
-      in 2023 and obtained my master's degree in computer science with honors.
-      Besides being a nerd, I like coffee, hiking, martial arts, Marvel, and
-      probably a lot more. Here are four facts about me:
-    </p>
+  <div class="flex justify-center">
+    <div class="flex flex-col xl:w-2/3 space-y-4 text-xl">
+      <img src="/assets/img/bio-photo.png" class="mx-auto w-1/2" />
+      <p>
+        Hi, my name is Brian Sinkovec and I'm an enthusiastic software engineer
+        from Aachen, Germany. I graduated from
+        <NuxtLink to="https://www.rwth-aachen.de"
+          >RWTH Aachen University
+        </NuxtLink>
+        in 2023 and obtained my master's degree in computer science with honors.
+        Besides being a nerd, I like coffee, hiking, martial arts, Marvel, and
+        probably a lot more. Here are four facts about me:
+      </p>
 
-    <ul class="list-inside list-disc">
-      <li>
-        Most interested in DevOps and security a.k.a.
-        <span class="font-bold">DevSecOps</span>
-      </li>
-      <li>
-        Proficient in Java + Spring Boot, cloud technologies like k8s, and a few
-        other things
-      </li>
-      <li>Advocates clean code, *-as-code, and to keep it simple.</li>
-      <li>Always forgets the fourth one...</li>
-    </ul>
+      <ul class="list-inside list-disc">
+        <li>
+          Most interested in DevOps and security a.k.a.
+          <span class="font-bold">DevSecOps</span>
+        </li>
+        <li>
+          Proficient in Java + Spring Boot, cloud technologies like k8s, and a
+          few other things
+        </li>
+        <li>Advocates clean code, *-as-code, and to keep it simple.</li>
+        <li>Always forgets the fourth one...</li>
+      </ul>
 
-    <p>
-      Feel free to contact me. Except you're a bot. Or a lizard. Then don't.
-    </p>
+      <p>
+        Feel free to contact me. Except you're a bot. Or a lizard. Then don't.
+      </p>
+    </div>
   </div>
 </template>
