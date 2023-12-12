c04ea0270861b8af67f277d16397340fa03c6d5e
Brian Sinkovec <bsin1807@gmail.com>
2023-12-12 16:40:29 +0100

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   typescript: { typeCheck: true },
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss'],
