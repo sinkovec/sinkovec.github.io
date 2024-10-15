a54f607f01d4a6e446a40b4cd08a39e7259e1129
Sinkovec, Brian (init) <brian.sinkovec@init.de>
2024-10-15 15:52:46 +0200

    switch to NuxtImg

--- components/resume/BioPhoto.vue
+++ components/resume/BioPhoto.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center">
-    <img
-      src="@/assets/img/bio-photo.png"
+    <NuxtImg
+      src="/img/bio-photo.png"
       class="self-center block w-44 h-44 p-4 rounded-full"
     />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col space-y-4 text-xl">
-    <img src="/assets/img/bio-photo.png" class="mx-auto w-1/2" />
+    <NuxtImg src="/img/bio-photo.png" class="mx-auto w-1/2" />
     <p>
       Hi, my name is Brian Sinkovec and I'm an enthusiastic software engineer
       from Aachen, Germany. I graduated from
