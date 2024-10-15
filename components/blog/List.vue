e5f6e580fab2f3f4fe1e74b15d7399de2db3bbce
Brian Sinkovec <bsin1807@gmail.com>
2024-10-15 16:11:38 +0200

    Slot templates for BlogList

--- components/blog/List.vue
+++ components/blog/List.vue
@@ -1,17 +1,28 @@
 <template>
   <main>
-    <ContentList v-slot="{ list }" :query="query">
-      <div class="flex flex-col gap-4">
-        <div v-for="post in list" :key="post._path" class="flex justify-center">
-          <NuxtLink :to="post._path">
-            <span
-              class="text-xl text-secondary hover:underline underline-offset-2 decoration-1 py-4"
-            >
-              {{ post.title }}
-            </span>
-          </NuxtLink>
+    <ContentList :query="query">
+      <template #default="{ list }">
+        <div class="flex flex-col gap-4">
+          <div
+            v-for="post in list"
+            :key="post._path"
+            class="flex justify-center"
+          >
+            <NuxtLink :to="post._path">
+              <span
+                class="text-xl text-secondary hover:underline underline-offset-2 decoration-1 py-4"
+              >
+                {{ post.title }}
+              </span>
+            </NuxtLink>
+          </div>
         </div>
-      </div>
+      </template>
+      <template #not-found>
+        <div class="flex justify-center">
+          <p>Coming soon...</p>
+        </div>
+      </template>
     </ContentList>
   </main>
 </template>
