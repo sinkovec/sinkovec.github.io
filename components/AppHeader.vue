5ade196fa17c5d0accd7ec3741ae978142a1e5fb
sinkovec <bsin1807@gmail.com>
2024-10-17 14:16:49 +0200

    Move navigation to NavBar component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -9,36 +9,6 @@
         </span>
       </NuxtLink>
     </div>
-    <nav class="content-center">
-      <ul class="flex gap-4">
-        <li v-for="nav of navigation" :key="nav.title">
-          <NuxtLink :to="nav.path">
-            <span
-              class="text-secondary hover:underline underline-offset-2 decoration-1"
-            >
-              {{ nav.title }}
-            </span>
-          </NuxtLink>
-        </li>
-      </ul>
-    </nav>
+    <NavBar/>
   </div>
 </template>
-
-<script lang="ts" setup>
-type NavigationItem = {
-  title: string
-  path: string
-}
-
-const navigation: NavigationItem[] = [
-  {
-    title: 'blog',
-    path: '/blog',
-  },
-  {
-    title: 'resume',
-    path: '/resume',
-  },
-]
-</script>
