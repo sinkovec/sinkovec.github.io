c7ed35c8b127b509803301f0e1a94d40dc5d7f28
sinkovec <bsin1807@gmail.com>
2025-09-01 12:59:15 +0200

    feat: Add letter page

---
+++ components/resume/Letter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-col p-8 gap-8 bg-slate-100 text-sm text-primary min-h-full">
+    <div class="flex flex-col italic text-right text-gray-400">
+      <span class="font-semibold not-italic">{{ basics.name }}</span>
+      <span>{{ basics.location.address }}</span>
+      <span>{{ basics.location.postalCode }} {{ basics.location.city }}</span>
+      <span>{{ basics.location.region }}</span>
+      <span>{{ basics.phone }}</span>
+      <span>{{ basics.email }}</span>
+      <NuxtLink
+        v-for="(item, index) in basics.profiles"
+        :key="index"
+        :to="item.url"
+        class="flex items-center gap-1.5 justify-end"
+      >
+        <Icon :name="item.icon" />
+        <span>{{ item.username }}</span>
+      </NuxtLink>
+    </div>
+    <div class="flex flex-col">
+      <span class="font-bold">{{ data.company.name }}</span>
+      <span>{{ data.company.contact.name }}</span>
+      <span>{{ data.company.location.address }}</span>
+      <span
+        >{{ data.company.location.postalCode }}
+        {{ data.company.location.city }}</span
+      >
+      <span>{{ data.company.location.region }}</span>
+    </div>
+    <div class="flex justify-between">
+      <span class="text-base font-bold">Application as {{ data.position }}</span>
+      <span class="italic">{{ moment().format('MMM D, YYYY') }}</span>
+    </div>
+    <span>Dear Sir or Madam,</span>
+    <p class="text-wrap whitespace-pre leading-5">{{ data.body }}</p>
+    <div class="flex flex-col text-sm">
+      <span>Best regards</span>
+      <span class="font-semibold">{{ basics.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import moment from 'moment'
+import resume from '../data/resume.yaml'
+import data from '../data/letter.yaml'
+
+const basics = resume.basics
+</script>
+
+<style></style>
---
+++ pages/letter.vue
@@ -0,0 +1,9 @@
+<template>
+  <ResumeLetter />
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'resume',
+})
+</script>
