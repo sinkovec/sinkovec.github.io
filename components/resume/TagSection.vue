e66edbae3e796903c6345e98d0c3aac9f49098b8
sinkovec <bsin1807@gmail.com>
2025-09-06 22:28:13 +0200

    chore: Update skill categories layout

--- components/resume/TagSection.vue
+++ components/resume/TagSection.vue
@@ -4,13 +4,15 @@
       :title="title"
       class="bg-slate-100 text-primary text-center"
     />
-    <div class="flex flex-col gap-1 pt-2">
+    <div class="flex flex-col gap-1 pt-2 text-xs">
       <div v-for="(item, index) in items" :key="index" class="flex flex-col">
-        <span class="px-4 font-light tracking-wider text-xs text-gray-300">
-          <Icon :name=item.icon class="translate-y-0.5"/>
-          {{ item.category }}
-        </span>
-        <div class="flex flex-wrap justify-center px-4 pt-1 pb-3 gap-1 text-xs">
+        <div class="mb-1">
+          <span class="px-4 py-0.5 font-light tracking-wider text-gray-300 bg-nord-1 rounded-r-xl text-sm">
+            <Icon :name=item.icon class="translate-y-0.5"/>
+            {{ item.category }}
+          </span>
+        </div>
+        <div class="flex flex-wrap justify-center px-4 pt-1 pb-3 gap-1">
           <Tag v-for="(tag, tagIndex) in item.tags" :key="tagIndex" class="px-2.5 py-0.5">
             <span>{{ tag }}</span>
           </Tag>
